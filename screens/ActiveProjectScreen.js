5fe31a21fc1cff009752da77ed4755e1fb65ef90
Gurban Gurbanov <qurban.qurbanov93@gmail.com>
2018-04-30 08:28:41 +0400

    Added plus button to active project screen

--- screens/ActiveProjectScreen.js
+++ screens/ActiveProjectScreen.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import {Header, Icon} from 'react-native-elements';
+import {Header} from 'react-native-elements';
 import {connect} from 'react-redux';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import HamburgerMenu from '../components/HamburgerMenu';
 
@@ -10,20 +11,36 @@ class ActiveProjectScreen extends Component {
         title: 'Active Project',
     };
 
+    addActiveProjectBtn = () => {
+        return (
+            <TouchableOpacity
+                onPress={() => {
+                    alert('Showing the appropriate form...');
+                }}
+            >
+                <Icon
+                    name="plus"
+                    color="white"
+                    size={22}
+                />
+            </TouchableOpacity>
+        );
+    };
+
     render() {
         return (
             <View>
                 <Header
                     leftComponent={<HamburgerMenu {...this.props}/>}
-                    centerComponent={{ text: ActiveProjectScreen.navigationOptions.title, style: { color: '#fff' } }}
+                    centerComponent={{text: ActiveProjectScreen.navigationOptions.title, style: {color: '#fff'}}}
+                    rightComponent={this.addActiveProjectBtn()}
                 />
             </View>
         );
     }
 }
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
 
 mapStateToProps = (state) => state;
 
